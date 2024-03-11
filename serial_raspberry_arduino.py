7cbdac20aa3cfa077e0846ce13a0a48414bdf3d5
Guilherme Tavares Pinheiro <115669765+GuiTavs7@users.noreply.github.com>
2024-03-11 15:29:02 -0300

    Update serial_raspberry_arduino.py

--- serial_raspberry_arduino.py
+++ serial_raspberry_arduino.py
@@ -1,5 +1,4 @@
 #Programa : Comunicacao Raspberry e Arduino
-#Autor : MakerHero
 
 #Execute os codigos abaixo no terminal do Raspbian para configurar a porta serial ttyACM0 corretamente
 #sudo systemctl mask serial-getty@ttyAMAO.service
@@ -13,24 +12,19 @@ import serial
 ser = serial.Serial("/dev/ttyACM0", 115200)
  
 GPIO.setmode(GPIO.BOARD)
- 
-#Define o pino do botao como entrada
-GPIO.setup(18, GPIO.IN)
- 
+
 #Mensagem inicial
-print ("Pressione o botao...")
+print ("Pressione a tecla L...")
  
 while(1):
-    #Verifica se o botao foi pressionado
-    if GPIO.input(18) == True:
-        #Envia o caracter L pela serial
-        data = 'L'.encode() #funcao para transformar string L em byte
-        ser.write(data) 
-        print('Enviado - L')
-        #Aguarda reposta
-        resposta = ser.readline()
-        #Mostra na tela a resposta enviada
-        #pelo Arduino
-        print (resposta)
-        #Aguarda 0,5 segundos e reinicia o processo
-        time.sleep(0.5)
+       #Envia o caracter L pela serial
+       data = 'L'.encode() #funcao para transformar string L em byte
+       ser.write(data) 
+       print('Enviado - L')
+       #Aguarda reposta
+       resposta = ser.readline()
+       #Mostra na tela a resposta enviada
+       #pelo Arduino
+       print (resposta)
+       #Aguarda 0,5 segundos e reinicia o processo
+       time.sleep(0.5)
